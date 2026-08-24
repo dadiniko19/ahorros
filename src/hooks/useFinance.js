cd6c1b7684b7e95d37de98e3649ac95c697da60c
Usuario <revoomarketing.col@gmail.com>
2026-08-24 15:09:46 -0500

    Export deductFixedExpense function from hook

--- src/hooks/useFinance.js
+++ src/hooks/useFinance.js
@@ -504,6 +504,7 @@ export const useFinance = () => {
     updateSalary,
     updateCreditBalance,
     deductArriendo,
+    deductFixedExpense,
     getTotalSalary,
     getTotalIncome,
     getTotalExpense,
